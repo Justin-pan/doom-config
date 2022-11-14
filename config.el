21cadf49c7029f7750217182194703b2d6957ed1
Justin Pan <justin.pan.westmount@gmail.com>
2022-11-13 18:47:45 -0800

    stuff

--- config.el
+++ config.el
@@ -24,8 +24,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "SauceCodePro NF" :size 15 :weight 'semi-light)
-    doom-variable-pitch-font (font-spec :family "sans" :size 15))
+(setq doom-font (font-spec :family "SauceCodePro NF" :size 20 :weight 'semi-light)
+    doom-variable-pitch-font (font-spec :family "sans" :size 20))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -84,7 +84,9 @@
 )
 (map! :leader
       (:prefix-map ("p" . "project")
-        :desc "Build current project" "b" #'make-without-asking))
+        :desc "Build current project" "B" #'make-without-asking))
+
+(setq company-global-modes 'nil)
 
 (define-key global-map "\t" 'dabbrev-expand)
 (define-key global-map [S-tab] 'indent-for-tab-command)
